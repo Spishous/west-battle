35cef89dce614a94d5f6b5d3a0bc7c21f3d0a931
Lucas Rayan <53474519+Spishous@users.noreply.github.com>
2024-05-02 16:40:41 +0200

    Add files via upload

---
+++ dist/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1714660806,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg_floor-sheet0.png",
+		"images/floor2-sheet0.png",
+		"images/sun-sheet0.png",
+		"images/particles.png",
+		"images/p1-sheet0.png",
+		"images/decor1-sheet0.png",
+		"images/decor2-sheet0.png",
+		"images/tubleweel-sheet0.png",
+		"images/shadow-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/battle west.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
